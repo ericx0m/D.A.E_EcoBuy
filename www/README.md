f6c60e3e955c5f3d6768899f99a712eb8412a915
branco <branco.hernandez@gmail.com>
2022-04-26 22:10:02 -0400

    avance esqueleto
    en este avance se desarrollo en el area del html css y js

---
+++ www/README.md
@@ -0,0 +1,2 @@
+# D.A.E_EcoBuy
+EcoBay es una aplicación para buscar las mejores ofertas 
---
+++ www/indexx.js
@@ -0,0 +1,17 @@
+const Request = require("request");
+const requestPromise = require("request-promise");
+const cheerio = require("cheerio");
+
+(async()=>{
+
+
+const response = await requestPromise(
+    "https://www.solotodo.cl/products/87689-sony-playstation-5-all-digital-glacier-white"
+);
+
+const $ =  cheerio.load(response);
+const title = $("table").text();
+
+console.log(table);
+    
+})();
